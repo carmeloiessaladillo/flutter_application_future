62e58e1e33d63c05e22f94d2eb03ea4abeb6f9a0
Carmelo Villegas cruz <meloville@gmail.com>
2022-01-04 17:55:13 +0100

    Initial commit

---
+++ lib/example_future.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ExampleFuture extends StatefulWidget {
+  const ExampleFuture({ Key? key }) : super(key: key);
+  
+  @override
+  _ExampleFutureState createState() => _ExampleFutureState();
+}
+
+
+class _ExampleFutureState extends State<ExampleFuture> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
