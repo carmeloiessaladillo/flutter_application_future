a2419c9170ed7c6d1eecb25195d19a1a5adcbfe3
Carmelo Villegas cruz <meloville@gmail.com>
2022-01-07 11:19:25 +0100

    Getting data from Rest API

--- lib/example_future.dart
+++ lib/example_future.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ExampleFuture extends StatefulWidget {
   const ExampleFuture({Key? key}) : super(key: key);
@@ -15,7 +19,22 @@ class _ExampleFutureState extends State<ExampleFuture> {
         title: const Text('FutureBuilder'),
         backgroundColor: Colors.teal,
       ),
-      body: Container(),
+      body: FutureBuilder(
+        future: http.get(Uri.parse('https://reqres.in/api/users')),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(child: CircularProgressIndicator());
+          }else{
+            if(snapshot.hasData){
+              final rawData = snapshot.data as http.Response;
+              final jsonMap = jsonDecode(rawData.body);
+              print(jsonMap['data']);
+            }
+            return Container();
+          }
+       
+        },
+      ),
     );
   }
 }
